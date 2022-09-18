59025b7371e41a0fa0872a208a837cebdccc58f1
Poyraz Avsever <poyrazavsever@hotmail.com>
2022-09-18 21:23:11 +0300

    chore: Main Title Update

--- src/Components/MainTitle.js
+++ src/Components/MainTitle.js
@@ -1,16 +1,16 @@
 import styled  from "styled-components"
 
 export const MainTitle = styled.h1`
-    font-size: 2.4rem;
-    font-weight: 500;
-    padding: 0.4rem 1.2rem;
-    width: 100%;
+    font-size: 2rem;
+    font-weight: 400;
+    padding: 0.4rem 1.6rem;
     position:relative;
     top:${props => props.top ? "-32px" :"0px"};
     text-align: ${props => props.center ? "center" : ""};
+    width: ${props => props.center ? "100%" : ""};
     display: inline-block;
     color: var(--white);
-    background-color:var(--primary);
+    background-color:${props => props.background ? "var(--primary)" : ""} ;
     span{
         color: var(--secondary);
     }
