e2f6b960dfd35608d1bc46e7e55e917283616c23
Poyraz Avsever <poyrazavsever@hotmail.com>
2022-09-20 19:33:16 +0300

    chore: update menu json

--- src/Cards.js
+++ src/Cards.js
@@ -52,59 +52,167 @@ export const galleryCard = [
 export const mainCourse = [
     {
         link:"Images/meat1.png",
+        name:"Beef",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"50$"
     },
     {
         link:"Images/meat2.png",
+        name:"Pork Chop",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"40$"
     },
     {
         link:"Images/meat3.png",
+        name:"Lamb Chop",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"80$"
     },
     {
         link:"Images/meat4.png",
+        name:"Turkey",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"24$"
+    },
+    {
+        link:"Images/salad.png",
+        name:"Salad",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"15$"
+    },
+    {
+        link:"Images/chicken.png",
+        name:"Chicken",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"42$"
+    },
+    {
+        link:"Images/chicken2.png",
+        name:"Chicken Legs",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"30$"
     },
 ]
 
 export const sweet = [
     {
-        link:"Images/meat2.png",
+        link:"Images/applepie.png",
+        name:"Apple Pie",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"10$"
     },
     {
-        link:"Images/meat4.png",
+        link:"Images/cake.png",
+        name:"Cake",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"25$"
     },
     {
-        link:"Images/meat1.png",
+        link:"Images/donut.png",
+        name:"Doughnut",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"18$"
     },
     {
-        link:"Images/meat3.png",
+        link:"Images/pudding.png",
+        name:"Pudding",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"24$"
+    },
+    {
+        link:"Images/jelly.png",
+        name:"Jelly",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"41$"
+    },
+    {
+        link:"Images/icecream.png",
+        name:"İcecream",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"80$"
     },
 ]
 
 export const drink = [
     {
-        link:"Images/meat4.png",
+        link:"Images/water.png",
+        name:"Water",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"70$"
     },
     {
-        link:"Images/meat3.png",
+        link:"Images/tea.png",
+        name:"Tea",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"35$"
     },
     {
-        link:"Images/meat2.png",
+        link:"Images/hotchocolate.png",
+        name:"Hot chocolate",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"75$"
     },
     {
-        link:"Images/meat1.png",
+        link:"Images/herbaltea.png",
+        name:"Herbal Tea",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"12$"
+    },
+    {
+        link:"Images/coke.png",
+        name:"Coke ",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"15$"
+    },
+    {
+        link:"Images/fruitjuice.png",
+        name:"Fruit juice",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"22$"
+    },
+    {
+        link:"Images/milkshake.png",
+        name:"Milkshake",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"45$"
     },
 ]
 
 export const alcohol = [
     {
-        link:"Images/meat3.png",
+        link:"Images/vodka.png",
+        name:"Vodka",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"240$"
     },
     {
-        link:"Images/meat4.png",
+        link:"Images/gin.png",
+        name:"Gin",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"180$"
     },
     {
-        link:"Images/meat1.png",
+        link:"Images/whiskey.png",
+        name:"Whiskey",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"250$"
     },
     {
-        link:"Images/meat2.png",
+        link:"Images/tequila.png",
+        name:"Tequila",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"144$"
+    },
+    {
+        link:"Images/brandy.png",
+        name:"Brandy",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"80$"
+    },
+    {
+        link:"Images/aperol.png",
+        name:"Aperol",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        fee:"100$"
     },
 ]
